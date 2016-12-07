92ac31646a556a796fb84a826ab91c751e6b9eb9
asemt <andreas.semt@gmx.net>
2016-12-07 11:13:27 +0100

    Colored DEBUG output for easier spotting.

--- debugging/debug.go
+++ debugging/debug.go
@@ -3,23 +3,30 @@ package debugging
 import (
 	"log"
 	"os"
+
+	"github.com/fatih/color"
 )
 
 type Debugging bool
 
-// flip to false to deactivate debug output
 var Debug Debugging
+
+// Colored DEBUG output for easy spotting.
+// Reference: https://github.com/fatih/color#insert-into-noncolor-strings-sprintfunc
+var yellowBold func(a ...interface{}) string
 
 func (d Debugging) Printf(format string, args ...interface{}) {
 	if d {
-		log.Printf("DEBUG >>  "+format, args...)
+		log.Printf(yellowBold("DEBUG >>  ")+format, args...)
 	}
 }
 
+// Activate debug output based on `DEBUG` environment variable.
 func init() {
 	hfDbg := os.Getenv("DEBUG")
 	if hfDbg != "" {
 		// the env var DEBUG is actually defined
 		Debug = true
+		yellowBold = color.New(color.Bold, color.FgYellow).SprintFunc()
 	}
 }
