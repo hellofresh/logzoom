8e70eac4cfdef13d96c05a1d596fe6d75eefc7b3
asemt <andreas.semt@gmx.net>
2016-12-07 11:14:10 +0100

    Reintroduce debug output and metrics.

--- output/elasticsearch/elasticsearch.go
+++ output/elasticsearch/elasticsearch.go
@@ -11,13 +11,111 @@ import (
 
 	//	"golang.org/x/net/context"
 
+	"strings"
+
 	"github.com/hellofresh/elastic"
 	"github.com/hellofresh/logzoom/buffer"
+	. "github.com/hellofresh/logzoom/debugging"
 	"github.com/hellofresh/logzoom/output"
 	"github.com/hellofresh/logzoom/route"
 	"github.com/paulbellamy/ratecounter"
+	"github.com/peterbourgon/g2s"
 	"gopkg.in/yaml.v2"
 )
+
+// StatsD metrics publishing.
+var statPub *StatsDCntPublisher
+var configuredESHost string
+
+type StatsDCntPublisher struct {
+	Client  *g2s.Statsd
+	enabled bool
+	prefix  string
+}
+
+// Depending on the value of 'addr' - which is taken from the environment variable "STATSD_ADDRESS" - we create
+// a functional or "dummy" instance of StatsDCntPublisher.
+func NewStatsDCntPublisher(proto, addr, prefix string) *StatsDCntPublisher {
+	sdm := StatsDCntPublisher{
+		Client:  nil,
+		enabled: false,
+		prefix:  prefix,
+	}
+	switch addr {
+	case "unset":
+		return &sdm
+	default:
+		statsdClnt, err := g2s.Dial(proto, addr)
+		if err != nil {
+			log.Println("Unable to connect to StatsD at defined address '%s'.\n", addr)
+		}
+		log.Printf("Successfully connected to StatsD at defined address '%s'.\n", addr)
+		sdm.Client = statsdClnt
+		sdm.enabled = true
+	}
+	return &sdm
+}
+
+// Convenience function for metrics publishing.
+func (p *StatsDCntPublisher) Cntr(name string, val int) {
+	if p.enabled {
+		p.Client.Counter(1.0, p.prefix+name, val)
+	}
+}
+
+// Helper function to extract ElasticSearch host for metrics publishing.
+func extractESHostPrfx(esHosts []string) string {
+	fstEsHost := esHosts[0]
+	inpWoSchema := strings.Split(fstEsHost, "//")[1]
+	fqdn := strings.Split(inpWoSchema, ":")[0]
+	return strings.Split(fqdn, ".")[0]
+}
+
+// Helper function to print bulk responses for debugging purposes.
+func printBulkResponse(r *elastic.BulkResponse) {
+	Debug.Printf("------------------------- bulk response ::BEGIN -------------------------")
+	Debug.Printf("blkRsp:\n")
+	Debug.Printf("...  Took: %d\n", r.Took)
+	Debug.Printf("...  Errors: %t\n", r.Errors)
+	Debug.Printf("...  Num of items: %d\n", len(r.Items))
+	Debug.Printf("...  Failed items   : %d\n", len(r.Failed()))
+	Debug.Printf("...  Succeeded items: %d\n", len(r.Succeeded()))
+
+	// explode `Items  []map[string]*BulkResponseItem`
+	for i, elm := range r.Items {
+		Debug.Printf("......  item no %d\n", i)
+		for key, val := range elm {
+			Debug.Printf("......  key: '%s'\n", key)
+			Debug.Printf("......  val: %#v\n", val)
+			// Do we have a non-nil elastic.ErrorDetails as part of elastic.BulkResponseItem?
+			errDtls := val.Error
+			if errDtls != nil {
+				Debug.Printf("......  error details:\n")
+				Debug.Printf(".........  CausedBy     : %v", errDtls.CausedBy)
+				Debug.Printf(".........  FailedShards : %v", errDtls.FailedShards)
+				Debug.Printf(".........  Grouped      : %t", errDtls.Grouped)
+				Debug.Printf(".........  Index        : %s", errDtls.Index)
+				Debug.Printf(".........  Phase        : %s", errDtls.Phase)
+				Debug.Printf(".........  Reason       : %s", errDtls.Reason)
+				Debug.Printf(".........  ResourceId   : %s", errDtls.ResourceId)
+				Debug.Printf(".........  ResourceType : %s", errDtls.ResourceType)
+				Debug.Printf(".........  RootCause    : %v", errDtls.RootCause)
+				Debug.Printf(".........  Type         : %s", errDtls.Type)
+			}
+		}
+	}
+	Debug.Printf("------------------------- bulk response ::END   -------------------------\n\n")
+}
+
+// Send metrics about messages saved in ElasticSearch (total/failed).
+func publishESMsgMetrics(esHost string, blkRsp *elastic.BulkResponse) {
+	statPub.Cntr(fmt.Sprintf("%s.messages.succeeded", esHost), len(blkRsp.Succeeded()))
+	// Publish each failed message as it's own error type metric as indicated by ElasticSearch.
+	for _, fItmVal := range blkRsp.Failed() {
+		statPub.Cntr(fmt.Sprintf("%s.messages.failed.%s", esHost, fItmVal.Error.Type), 1)
+	}
+
+}
 
 const (
 	defaultHost        = "127.0.0.1"
@@ -60,6 +158,14 @@ type ESServer struct {
 
 func init() {
 	output.Register("elasticsearch", New)
+
+	// StatsD metrics publishing.
+	statsDAddrFromEnv := os.Getenv("STATSD_ADDRESS")
+	if statsDAddrFromEnv == "" {
+		statPub = NewStatsDCntPublisher("udp", "unset", "logcollector.")
+	} else {
+		statPub = NewStatsDCntPublisher("udp", statsDAddrFromEnv, "logcollector.")
+	}
 }
 
 func New() output.Output {
@@ -109,6 +215,9 @@ func (e *ESServer) ValidateConfig(config *Config) error {
 		return errors.New("Missing index type (e.g. logstash)")
 	}
 
+	// Hack to expose ElasticSearch hosts for metrics publishing.
+	configuredESHost = extractESHostPrfx(e.hosts)
+
 	return nil
 }
 
@@ -128,6 +237,9 @@ func (e *ESServer) Init(name string, config yaml.MapSlice, b buffer.Sender, rout
 	e.config = *esConfig
 	e.hosts = esConfig.Hosts
 	e.b = b
+
+	// Hack to expose the ElasticSearch host for metrics publishing.
+	configuredESHost = extractESHostPrfx(e.hosts)
 
 	return nil
 }
@@ -167,6 +279,12 @@ func (es *ESServer) insertIndexTemplate(client *elastic.Client) error {
 func (es *ESServer) afterCommit(id int64, requests []elastic.BulkableRequest, response *elastic.BulkResponse, err error) {
 	if es.idx.RateCounter.Rate() > 0 {
 		log.Printf("Flushed events to Elasticsearch, current rate: %d/s", es.idx.RateCounter.Rate())
+	}
+	if response != nil {
+		// Display debug output if DEBUG env var is set.
+		printBulkResponse(response)
+		// Publish metrics if STATSD_ADDRESS env var is set.
+		publishESMsgMetrics(configuredESHost, response)
 	}
 }
 
